0055c0718cfa7bd4c311b10019a8a142b8fd6278
Gubernik Vladislav <vgubernik@gmail.com>
2019-02-18 09:19:30 +0300

    Добавлены тесты

--- DbModule/src/test/java/ru/gubernik/weather/dbmodule/service/dao/WeatherDaoTest.java
+++ DbModule/src/test/java/ru/gubernik/weather/dbmodule/service/dao/WeatherDaoTest.java
@@ -16,6 +16,10 @@ import ru.gubernik.weather.dbmodule.model.Weather;
 import ru.gubernik.weather.dbmodule.model.Wind;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.Iterator;
 import java.util.List;
 
@@ -90,5 +94,28 @@ public class WeatherDaoTest {
         weatherDao.update(weather);
 
         verify(entityManager, times(1)).merge(weather);
+    }
+
+    @Test
+    public void getTest(){
+        CriteriaBuilder criteriaBuilder = mock(CriteriaBuilder.class);
+        CriteriaQuery<Weather> criteriaQuery = mock(CriteriaQuery.class);
+        Root root = mock(Root.class);
+        Weather weather = mock(Weather.class);
+        TypedQuery query = mock(TypedQuery.class);
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+        when(criteriaBuilder.createQuery(Weather.class)).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Weather.class)).thenReturn(root);
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(query);
+        when(query.getSingleResult()).thenReturn(weather);
+
+        weatherDao.get("");
+
+        verify(entityManager, times(1)).getCriteriaBuilder();
+        verify(criteriaBuilder, times(1)).createQuery(Weather.class);
+        verify(criteriaQuery, times(1)).from(Weather.class);
+        verify(entityManager, times(1)).createQuery(criteriaQuery);
+        verify(query, times(1)).getSingleResult();
     }
 }
---
+++ DbModule/src/test/java/ru/gubernik/weather/dbmodule/service/dao/spring/SpringWeatherDaoTest.java
@@ -0,0 +1,60 @@
+package ru.gubernik.weather.dbmodule.service.dao.spring;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import ru.gubernik.weather.dbmodule.dao.spring.SpringWeatherDaoImpl;
+import ru.gubernik.weather.dbmodule.model.Weather;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SpringWeatherDaoTest {
+
+    @Mock
+    private EntityManager entityManager;
+
+    @InjectMocks
+    private SpringWeatherDaoImpl weatherDaoTest;
+
+    @Test
+    public void checkNull(){
+        assertNotNull(entityManager);
+        assertNotNull(weatherDaoTest);
+    }
+
+    @Test
+    public void getTest(){
+        CriteriaBuilder criteriaBuilder = mock(CriteriaBuilder.class);
+        CriteriaQuery<Weather> criteriaQuery = mock(CriteriaQuery.class);
+        Root root = mock(Root.class);
+        Weather weather = mock(Weather.class);
+        TypedQuery query = mock(TypedQuery.class);
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+        when(criteriaBuilder.createQuery(Weather.class)).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Weather.class)).thenReturn(root);
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(query);
+        when(query.getSingleResult()).thenReturn(weather);
+
+        weatherDaoTest.get("");
+
+        verify(entityManager, times(1)).getCriteriaBuilder();
+        verify(criteriaBuilder, times(1)).createQuery(Weather.class);
+        verify(criteriaQuery, times(1)).from(Weather.class);
+        verify(entityManager, times(1)).createQuery(criteriaQuery);
+        verify(query, times(1)).getSingleResult();
+    }
+}
